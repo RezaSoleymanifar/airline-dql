b5375c0046a0b397bca0fd3f8d86fe08297d7077
Reza Soleymanifar <reza@soleymanifar.com>
2020-10-22 15:08:37 -0500

    add random event generator

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # airline-dql
-This project deals with creating an algorithm for making on the fly overbooking decisions in airline networks, based on realistic constraints.
+This project deals with creating an algorithm for making on the fly overbooking 
+decisions in airline networks, based on realistic constraints.
+
--- airline_network.py
+++ airline_network.py
@@ -5,14 +5,24 @@ import numpy as np
 
 n_nodes = 4
 n_legs = 4
+n_jrnys = 16
 arrival_probs = []
 cancel_probs = []
 no_show_probs = []
+
 leg_caps = []
-n_jrnys = 16
+
 jrny_fares = []
 no_show_sum = []
 cancel_sum = []
 
 state = np.zeros(n_jrnys)
 
+def gen_event(arrival_probs, cancel_probs):
+    no_vent_prob = 1 - (np.sum(arrival_probs), np.sum(cancel_probs))
+    probs = []
+    # make a matix of event indices and change to state
+    elements = [1.1, 2.2, 3.3]
+    probabilities = [0.2, 0.5, 0.3]
+    event = np.random.choice(elements, 10, p=probabilities)
+    return event
